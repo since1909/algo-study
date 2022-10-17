6dd0d28190ddc7c6594dc87f5cba86eb7c972cae
since1909 <since1909@daum.net>
2022-10-17 23:11:52 +0900

    boj18108

---
+++ src/main/java/Boj18108.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj18108 {
+    public void boj18108() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) - 543);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        bojSol.boj15591(args);
+        //bojSol.boj15591(args);
+        Boj18108 sol = new Boj18108();
+        sol.boj18108();
     }
 }
